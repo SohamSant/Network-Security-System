ea56a05a2a0b5c11b23352451c18618859758969
Soham Sant <sohamsant9@gmail.com>
2025-12-01 20:45:18 -0700

    Logging and Exception Handling

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m-%d-%Y_%H-%M-%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs")
+
+os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler()
+    ]
+)
+
+
